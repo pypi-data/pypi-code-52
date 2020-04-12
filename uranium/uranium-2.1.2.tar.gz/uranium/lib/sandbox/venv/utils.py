f9897ad80f2c0c8c68d5078d7b47ff4dd32b4cc3
Tom Forbes <tom@tomforb.es>
2020-04-12 03:36:32 +0000

    {"name":"uranium","version":"2.1.2","file":"uranium-2.1.2.tar.gz","path":"uranium/uranium-2.1.2.tar.gz"}

---
+++ uranium/uranium-2.1.2.tar.gz/uranium/lib/sandbox/venv/utils.py
@@ -0,0 +1,39 @@
+import logging
+import os
+import subprocess
+import sys
+import virtualenv
+from .activate_this import write_activate_this
+
+LOGGER = logging.getLogger(__name__)
+
+PREFIX = getattr(sys, "prefix", None)
+REAL_PREFIX = getattr(sys, "real_prefix", None)
+
+
+def install_virtualenv(install_dir):
+    if is_virtualenv(install_dir):
+        return
+
+    subprocess.call(
+        [
+            sys.executable,
+            virtualenv.__file__.rstrip("c"),
+            "--no-site-packages",
+            "--always-copy",
+            install_dir,
+        ]
+    )
+    write_activate_this(install_dir)
+
+
+VIRTUALENV_FILES = {"activate file": os.path.join("bin", "activate")}
+
+
+def is_virtualenv(path):
+    """ validate if the path is already a virtualenv """
+    for name, venv_path in VIRTUALENV_FILES.items():
+        target_path = os.path.join(path, venv_path)
+        if not os.path.exists(target_path):
+            return False
+    return True
